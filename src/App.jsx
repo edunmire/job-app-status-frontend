e5f7062b810478dd271326cd9b17d6038f5e6f1b
edunmire <edunmire@ucsd.edu>
2025-08-18 15:10:32 -0700

    Added job card placeholders and added basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,45 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const JobCard = ({ title }) => {
+  const [jobStatus, setJobStatus] = useState("watching");
+
+  useEffect(() => {
+    console.log(`${title} has been changed to ${jobStatus}`);    
+  })
 
   return (
+    <div className="jobcard">
+      <h2>{title}</h2>
+
+      <select className="status-select" onChange={(e) => setJobStatus(e.target.value)}>
+        <option value="watching">Watching</option>
+        <option value="applied">Applied</option>
+        <option value="rejected">Rejected</option>
+        <option value="accepted">Accepted</option>
+      </select>
+      
+    </div>
+  )
+}
+
+const App = () => {
+  return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>Current Job Applications</h1>
+
+    <div className='jobcard-container'>
+      <JobCard title='Oracle Design Intern'/>
+      <JobCard title='SWE Apple Engineer II'/>
+      <JobCard title='God Help Me! I cant'/>
+      <JobCard title='Dont stop believin'/>
+    </div>
+
+    <div>
+      <button id="new-job-button" onClick={()=>{console.log("Add new job")}}>+</button>
+    </div>
     </>
   )
 }
